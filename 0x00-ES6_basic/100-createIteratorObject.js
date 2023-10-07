58cbaa7d17ad1636cecb982f74f1d023a68728fd
code-lova <ebinuga@hotmail.com>
2023-10-07 14:26:14 +0100

    update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,10 @@
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
+  const employee = [];
+  // eslint-disable-next-line no-unused-vars, prefer-const
+  for (let [depts, emplys] of Object.entries(report.allEmployees)) {
+    for (const empy of emplys) {
+      employee.push(empy);
+    }
   }
+  return employee;
 }
